7466cb0efc63dd5fefdd922f55bcb303d712ddc6
Ahmad Jarara <git@ajarara.io>
2021-02-17 19:28:50 -0500

    Write test, put in table
    Using an emacs kbd macro worked _really_ well. I wrote it out by hand
    the first time I did this, it was error prone, and slow as all hell.

--- test/setOne.test.ts
+++ test/setOne.test.ts
@@ -2,6 +2,9 @@ import { setOne } from '../src';
 
 describe('setOne', () => {
   it('challenge one', () => {
-    expect(setOne.hexToBase64()).toBeUndefined();
+    const hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d';
+    const base64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t';
+    
+    expect(setOne.challengeOne(hex)).toEqual(base64);
   });
 });
