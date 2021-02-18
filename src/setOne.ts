0b95a13a9f4fc34baf9ec86945a006bdcef18d03
Ahmad Jarara <git@ajarara.io>
2021-02-17 19:05:29 -0500

    Fix lint

--- src/setOne.ts
+++ src/setOne.ts
@@ -1,3 +1,3 @@
 export const setOne = {
   hexToBase64: () => undefined,
-}
+};
--- test/setOne.test.ts
+++ test/setOne.test.ts
@@ -3,5 +3,5 @@ import { setOne } from '../src';
 describe('setOne', () => {
   it('challenge one', () => {
     expect(setOne.hexToBase64()).toBeUndefined();
-  })
+  });
 });
