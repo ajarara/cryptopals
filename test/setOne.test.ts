9adb127d40e9ca5848ad3385b452ba2b0c9b463b
Ahmad Jarara <git@ajarara.io>
2021-02-18 10:04:33 -0500

    Prettier pass

--- test/setOne.test.ts
+++ test/setOne.test.ts
@@ -2,9 +2,11 @@ import { setOne } from '../src';
 
 describe('setOne', () => {
   it('challenge one', () => {
-    const hex = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d';
-    const base64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t';
-    
+    const hex =
+      '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d';
+    const base64 =
+      'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t';
+
     expect(setOne.challengeOne(hex)).toEqual(base64);
   });
 });
